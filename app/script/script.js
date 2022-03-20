58a45fbfe16a3e3163b22007629c6e9808afd551
Shahid Afridi <shafridi9807@gmail.com>
2022-03-21 04:45:15 +0530

    projectPopup

--- app/script/script.js
+++ app/script/script.js
@@ -1,21 +1,24 @@
 const hambergerBtn = document.getElementById('hambergerButton')
 const hambergerMenu = document.getElementById('hambergerMenu')
-
 hambergerBtn.addEventListener('click', handleHamberger)
 
 function handleHamberger(){
     hambergerBtn.classList.toggle("active")
     if(hambergerBtn.classList.contains("active")){
         hambergerMenu.classList.add("active")
+        document.body.style.overflow = "hidden"
     }
     else{
         hambergerMenu.classList.remove("active")
+        document.body.style.overflow = "scroll"
     }
 }
 
 const bookmark = document.querySelector('.bookmark-container')
 let bookmarkText = document.getElementById("bookmarkText")
-bookmark.addEventListener('click', ()=> {
+bookmark.addEventListener('click', handleBookmark)
+
+function handleBookmark(){
     bookmark.classList.toggle("active")
     if(bookmark.classList.contains("active")){
         bookmarkText.innerText = "Bookmarked"
@@ -23,5 +26,44 @@ bookmark.addEventListener('click', ()=> {
     else{
         bookmarkText.innerText = "Bookmark"
     }
+}
+
+const projectBtn = document.getElementById('projectBtn')
+const projectMenu  = document.querySelector('.project-selector')
+const projectCancelBtn = document.getElementById('projectCancelBtn')
+projectBtn.addEventListener('click', handleProject)
+projectCancelBtn.addEventListener('click', handleProjectCancel)
+
+function handleProject(){
+    projectMenu.classList.add("active")
+}
+
+function handleProjectCancel(){
+    projectMenu.classList.remove("active")
+}
+
+const projectSelector = document.querySelectorAll('.SelectedCard')
+projectSelector.forEach(element =>{
+    
+    element.addEventListener('click', handleProjectSelector)
 })
 
+function handleProjectSelector(event){
+    projectSelector.forEach(card => {
+        card.classList.remove("active")
+    })
+    event.currentTarget.classList.add("active")
+    event.currentTarget.querySelector('.text-box .title input').checked = true
+    event.currentTarget.querySelector('.input-box button').addEventListener('click', handleContinue)
+
+}
+const thanksMsg = document.getElementById('thanksMsg')
+function handleContinue(){
+    projectMenu.classList.remove("active")
+    thanksMsg.classList.add("active")
+}
+const thanksGotIt = document.getElementById('thanksGotIt')
+
+thanksGotIt.addEventListener('click', ()=>{
+    thanksMsg.classList.remove("active")
+})
